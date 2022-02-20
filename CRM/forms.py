fc8647eb887f2112c1d2bd5eff2159a6d557891c
ManyuDhyani <manyudhyani@gmail.com>
2022-02-21 00:55:35 +0530

    hidden value fix: Lead

--- CRM/forms.py
+++ CRM/forms.py
@@ -6,7 +6,6 @@ class LeadForm(forms.ModelForm):
     # def send_email(self):
     #     # send email using the self.cleaned_data dictionary
     #     pass
-    status = forms.CharField(widget = forms.HiddenInput(), required = False, initial="Lead")
 
     class Meta:
         model = Customer
@@ -16,9 +15,6 @@ class LeadForm(forms.ModelForm):
     
     def __init__(self, *args, **kwargs):
         super(LeadForm, self).__init__(*args, **kwargs)
-        self.fields['gender'].widget.attrs.update({"class": "form-control"})
-    #     self.fields['first_name'].widget.attrs.update({'class': 'border border-2 border-primary rounded'})
-    #     self.fields['last_name'].widget.attrs.update({'class': 'border border-2 border-primary rounded'})
         self.fields['gender'].widget.attrs.update({'class': 'border border-2 border-primary rounded'})
         self.fields['events'].widget.attrs.update({'class': 'border border-2 border-primary rounded'})
     #     self.fields['phone_number'].widget.attrs.update({'class': 'border border-2 border-primary rounded'})
