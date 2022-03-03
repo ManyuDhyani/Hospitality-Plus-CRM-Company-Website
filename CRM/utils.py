22f90dbf1c8fdaddeb14076e144e56c87a23ec5a
ManyuDhyani <manyudhyani@gmail.com>
2022-03-03 23:16:41 +0530

    Integrated Graphs to Admin DashBoard: Lead Conversion, Top Trending categories, Gender and Lead vs Customer Charts

--- CRM/utils.py
+++ CRM/utils.py
@@ -1,5 +1,6 @@
 from .models import Customer
 from django.db.models import Count
+import calendar
 
 def pie_chart_gender_ratio():
     labels = []
@@ -38,7 +39,65 @@ def bar_chart_lead_customer_ratio():
 
     context = {
         'labelsLeadCustomer': labels,
-        'datalLeadCustomer': data,
+        'dataLeadCustomer': data,
     }
 
+    return context
+
+
+def bar_chart_trend_category():
+    labels = []
+    data = []
+    context = {}
+
+    queryset = Customer.objects.values('category_attended__title').annotate(trend_count=Count('category_attended__title')).order_by('-trend_count')[:7]
+    
+    for i, cat in enumerate(queryset):
+        x, y = list(queryset[i].values())
+        if x is None:
+            continue
+        labels.append(x)
+        data.append(y)
+        
+    context = {
+        'labelsTrendCategories': labels,
+        'dataTrendCategories': data,
+    }
+
+    return context
+
+
+def Customer_Lead_By_Month():
+    labelsCustomer = []
+    dataCustomer = []
+    labelsLead = []
+    dataLead = []
+    context = {}
+
+    querysetCustomer = Customer.objects.values('date_added__month').annotate(Count('status')).order_by('date_added__month').filter(status='Customer')[:4]
+    
+    querysetLead = Customer.objects.values('date_added__month').annotate(Count('status')).order_by('date_added__month').filter(status='Lead')[:4]
+    
+
+    for i, _ in enumerate(querysetCustomer):
+        x, y = list(querysetCustomer[i].values())
+        if x is None:
+            continue
+        labelsCustomer.append(calendar.month_name[x])
+        dataCustomer.append(y)
+    
+    for i, _ in enumerate(querysetLead):
+        x, y = list(querysetLead[i].values())
+        if x is None:
+            continue
+        labelsLead.append(calendar.month_name[x])
+        dataLead.append(y)
+
+    context = {
+        'labelsCustomer': labelsCustomer,
+        'dataCustomer': dataCustomer,
+        'labelsLead': labelsLead,
+        'dataLead': dataLead
+    }
+    
     return context
--- Dashboard/views.py
+++ Dashboard/views.py
@@ -1,11 +1,13 @@
 from multiprocessing import context
 from django.shortcuts import render
-from CRM.utils import pie_chart_gender_ratio, bar_chart_lead_customer_ratio
+from CRM.utils import pie_chart_gender_ratio, bar_chart_lead_customer_ratio, bar_chart_trend_category, Customer_Lead_By_Month
 
 def dashboard(request):
     context = pie_chart_gender_ratio()
     context.update(bar_chart_lead_customer_ratio())
-    # print(context)
+    context.update(bar_chart_trend_category())
+    context.update(Customer_Lead_By_Month())
+    
     return render(request, "dashboard.html", context)
 
 
