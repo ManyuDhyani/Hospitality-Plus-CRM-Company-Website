c071a12ad2a9e9d35aef228948c1cee76874dacb
ManyuDhyani <manyudhyani@gmail.com>
2022-04-13 19:47:48 +0530

    Added the Unique Slug generator

--- Activities/models.py
+++ Activities/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.db.models.signals import pre_save
+from .utils import unique_slug_generator_title
 
 class ActivitiesCategories(models.Model):
     category = models.CharField(max_length=40)
@@ -11,18 +13,23 @@ class ActivitiesCategories(models.Model):
 
 
 class Activities(models.Model):
-    name = models.CharField(max_length=40)
-    slug = models.SlugField(max_length=100, null=True, blank=True)
+    title = models.CharField(max_length=150)
+    slug = models.SlugField(max_length=250, null=True, blank=True)
     thumbnail = models.ImageField(upload_to='upload/treks')
     recommended = models.BooleanField(null=True, blank=True, default=False)
     altitude = models.CharField(max_length=20)
     duration_days = models.CharField(max_length=20)
     difficulty = models.CharField(max_length=20)
-    district = models.CharField(max_length=40)
+    location = models.CharField(max_length=40)
     distance = models.CharField(max_length=20)
     price = models.CharField(max_length=20)
-    description = models.TextField(max_length=150)
+    description = models.TextField(max_length=250)
     category = models.ForeignKey(ActivitiesCategories, on_delete=models.SET_NULL, null=True)
 
     class Meta:
         verbose_name_plural = 'Activities'
+
+    def __str__(self):
+        return self.title
+
+
